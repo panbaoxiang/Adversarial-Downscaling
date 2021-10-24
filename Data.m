d2f5e65872f7fc3fd161f3f7e74a40284b277986
lambda <78629642+panbaoxiang@users.noreply.github.com>
2021-10-24 16:05:52 -0700

    Update Data.m

--- Data.m
+++ Data.m
@@ -1,19 +1,12 @@
+SetDirectory["/Volumes/lambda/CFSR"];
 vars={"z1000","z850","z500","z200","q925","q850","q500","prmsl"};
 
 dates=DateRange[{1979,1,1},{2011,3,1},"Month"];
-
-(*SetDirectory["/Users/lambda/Documents/ADownscaling/Data/"];*)
-
-SetDirectory["/Volumes/lambda/CFSR"];
-
 script=Flatten[Table[Block[{date=DateString[dates[[i]],{"Year","Month"}]},"wget -nc https://www.ncei.noaa.gov/data/climate-forecast-system/access/reanalysis/time-series/"<>date<>"/"<>vars[[j]]<>".gdas."<>date<>".grb2"],{i,Length[dates]},{j,Length[vars]}]];
-
 Export["reanalysis_1979-2011.sh",script,"Table"]
 
 dates=DateRange[{2011,4,1},{2020,9,1},"Month"];
-
 script=Flatten[Table[Table[DateString[dates[[i]],{"wget -nc https://www.ncei.noaa.gov/data/climate-forecast-system/access/operational-analysis/time-series/","Year","/","Year","Month","/",vars[[j]],".gdas.","Year","Month",".grib2"}],{j,Length[vars]}],{i,Length[dates]}]];
-
 Export["reanalysis_2011-2021.sh",script,"Table"]
 
 <<JLink`;
