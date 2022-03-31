d0f33c8bbb83c2da8b1a031f7c5f2772ca4183c5
lambda <78629642+panbaoxiang@users.noreply.github.com>
2022-03-31 09:18:51 +0800

    Update Evaluation.m

--- Evaluation.m
+++ Evaluation.m
@@ -12,6 +12,11 @@ var={"pressfc","prmsl","pwat","q500","q850","q925","tmpsfc","u200","u850","v200"
 
 dynamics=Table[Block[{},Print[months[[i]]];Import[DateString[months[[i]],{"D_CONUS_","Year","_","Month",".mx"}]]["data"]],{i,Length[months]}];
 
+
+
+net=SRNet=Import["/data/home/scy0446/run/Code/trained_Res.mx"];
+
+
 validation=Block[{select=30,day},
   day=Table[RandomSample[Range[Length[p[[i,1]]]],select],{i,-24,-1}];
   Flatten[Table[<|"Dynamics"->dynamics[[i,;;,(day[[i,j]]-1)*3+1;;day[[i,j]]*3]],
@@ -21,18 +26,18 @@ validation=Block[{select=30,day},
     "P_2"->p[[i,3,day[[i,j]]]],
     "P_3"->p[[i,4,day[[i,j]]]]|>,{j,select},{i,-24,-1}]]];
 
-net=SRNet=Import["/data/home/scy0446/run/Code/trained_Res.mx"];
+
 obserP0=validation[[;;,"P_0"]];
-        obserP1=validation[[;;,"P_1"]];
-        obserP2=validation[[;;,"P_2"]];
-        obserP3=validation[[;;,"P_3"]];
-        simu=Map[net[<|"Dynamics"->#[["Dynamics"]],"Static"->#[["Static"]]|>,TargetDevice->"GPU"]&,validation];
-        {simuP0,simuP1,simuP2,simuP3}=Map[simu[[;;,#]]&,{"P_0","P_1","P_2","P_3"}];
-        {simuP0,simuP1,simuP2,simuP3}=Normal[{simuP0,simuP1,simuP2,simuP3}];
-        {obserP0,obserP1,obserP2,obserP3}=Normal[{obserP0,obserP1,obserP2,obserP3}];
-        mse={Mean[Flatten[(simuP0-obserP0)^2]],Mean[Flatten[(simuP1-obserP1)^2]],
+obserP1=validation[[;;,"P_1"]];
+obserP2=validation[[;;,"P_2"]];
+obserP3=validation[[;;,"P_3"]];
+simu=Map[net[<|"Dynamics"->#[["Dynamics"]],"Static"->#[["Static"]]|>,TargetDevice->"GPU"]&,validation];
+{simuP0,simuP1,simuP2,simuP3}=Map[simu[[;;,#]]&,{"P_0","P_1","P_2","P_3"}];
+{simuP0,simuP1,simuP2,simuP3}=Normal[{simuP0,simuP1,simuP2,simuP3}];
+{obserP0,obserP1,obserP2,obserP3}=Normal[{obserP0,obserP1,obserP2,obserP3}];
+mse={Mean[Flatten[(simuP0-obserP0)^2]],Mean[Flatten[(simuP1-obserP1)^2]],
              Mean[Flatten[(simuP2-obserP2)^2]],Mean[Flatten[(simuP3-obserP3)^2]]};
-        Print[mse]
+Print[mse]
 
 corr=Quiet[{Table[Correlation[simuP0[[;;,1,i,j]],obserP0[[;;,1,i,j]]],{i,Dimensions[simuP0][[3]]},{j,Dimensions[simuP0][[4]]}],
                       Table[Correlation[simuP1[[;;,1,i,j]],obserP1[[;;,1,i,j]]],{i,Dimensions[simuP1][[3]]},{j,Dimensions[simuP1][[4]]}],
